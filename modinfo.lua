75f63b3515bc0d82a5ac6702322bb95b072df443
gajop <gajo01@gmail.com>
2013-09-29 17:43:37 +0200

    initial commit

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+return {
+  name='Scenario Editor ZK',
+  description='Scenario Editor for Zero-K',
+  shortname='SE ZK',
+  version='0.1',
+  mutator='Official',
+  game='Scenario Editor ZK',
+  shortGame='SE ZK',
+  modtype=1,
+  depend = {  	
+	'Scenario Editor Core 0.1',
+	'Zero-K v1.1.9.10'
+  },
+}
