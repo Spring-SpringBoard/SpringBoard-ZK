40f8a73a12e8f7f419808cb71370b84a7b7ce5a3
Gajo Petrovic <gajo01@gmail.com>
2013-11-04 14:58:08 +0100

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -2,13 +2,13 @@ return {
   name='Scenario Editor ZK',
   description='Scenario Editor for Zero-K',
   shortname='SE ZK',
-  version='0.1',
+  version='0.2',
   mutator='Official',
   game='Scenario Editor ZK',
   shortGame='SE ZK',
   modtype=1,
   depend = {  	
-	'Scenario Editor Core 0.1',
+	'Scenario Editor Core 0.2',
 	'Zero-K v1.1.9.10'
   },
 }
