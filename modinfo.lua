963dc6fc3402a7154261d539e3dfb8a518466695
gajop <gajo01@gmail.com>
2014-07-05 19:04:46 +0200

    updated to work with ZK (have to use custom api_chili.lua to override ZK's)

--- modinfo.lua
+++ modinfo.lua
@@ -2,13 +2,13 @@ return {
   name='Scenario Editor ZK',
   description='Scenario Editor for Zero-K',
   shortname='SE ZK',
-  version='0.2+',
+  version='git',
   mutator='Official',
   game='Scenario Editor ZK',
   shortGame='SE ZK',
   modtype=1,
   depend = {  			
-	'Zero-K test-13556',
-	'Scenario Editor Core 0.2+',
+  	'Zero-K v1.2.7.3',
+  	'Scenario Editor Core git',
   },
 }
