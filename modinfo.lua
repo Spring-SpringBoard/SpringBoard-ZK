30e7dbfc41b6449ea0a4827258b3e5521b6d2932
Gajo Petrovic <gajo01@gmail.com>
2017-02-21 16:39:14 +0900

    Scened->SpringBoard

--- modinfo.lua
+++ modinfo.lua
@@ -1,14 +1,12 @@
 return {
-  name='Scened ZK',
-  description='Scened for Zero-K',
-  shortname='SE_ZK',
+  name='SpringBoard ZK',
+  description='SpringBoard for Zero-K',
+  shortname='SB_ZK',
   version='git',
   mutator='Official',
-  game='Scened ZK',
-  shortGame='SE_ZK',
   modtype=1,
   depend = {  			
   	'rapid://zk:stable',
-  	'Scened Core git'
+  	'SpringBoard Core git'
   },
 }
