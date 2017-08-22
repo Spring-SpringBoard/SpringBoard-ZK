36368e39e9842624b77f3735bfb78c5ccae06edf
Gajo Petrovic <gajo01@gmail.com>
2017-08-22 20:22:20 +0900

    enable/disable widgets explicitly; close https://github.com/Spring-SpringBoard/SpringBoard-Core/issues/270

--- sb_settings.lua
+++ sb_settings.lua
@@ -1,6 +1,24 @@
+local UIWidgets = {"EPIC Menu", "Chili Crude Player List", "Chili Core Selector", "Chili Economy Panel Default",
+                   "Chili Global Commands", "Chili EndGame Window", "Chili Integral Menu",
+                   "Chili Minimap", "Chili Pro Console", "Chili Selections & CursorTip v2"}
+
 return {
     startStop = {
         x = "48.5%",
         y = 52,
     },
+
+    OnStopEditingUnsynced = function()
+        for _, widgetName in ipairs(UIWidgets) do
+            widgetHandler:EnableWidget(widgetName)
+        end
+    end,
+
+    OnStartEditingUnsynced = function()
+        for _, widgetName in ipairs(UIWidgets) do
+            widgetHandler:DisableWidget(widgetName)
+        end
+        Spring.SendCommands("tooltip 0")
+        gl.SlaveMiniMap(true)
+    end,
 }
